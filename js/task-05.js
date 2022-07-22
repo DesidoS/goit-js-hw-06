4b9bd74af4cd87ff2f6ccf0379f122c94ecc158d
Oleksii Kravchenko <djdesidos@gmail.com>
2022-07-22 22:33:15 +0300

    add

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,6 @@ const inputEl = document.querySelector('#name-input');
 const outputEl = document.querySelector('#name-output');
 
 inputEl.addEventListener('input', (event) => {
-    outputEl.textContent = event.currentTarget.value
-    if (event.currentTarget.value === '') {
-        outputEl.textContent = "Anonymous";
-    }
+    event.currentTarget.value === '' ? outputEl.textContent = "Anonymous" : outputEl.textContent = event.currentTarget.value;
 }
 )
